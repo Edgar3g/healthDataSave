82782d4673cd017c2cec4962cf9303717d4ecdc1
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-09-03 19:21:18 +0100

    build(build.gradle): add all initial dependencies to start the project
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

---
+++ healthDataSave/src/main/java/hds/healthDataSave/api/HealthDataSaveApplication.java
@@ -0,0 +1,13 @@
+package hds.healthDataSave.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDataSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDataSaveApplication.class, args);
+	}
+
+}
---
+++ healthDataSave/src/test/java/hds/healthDataSave/api/HealthDataSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package hds.healthDataSave.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDataSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
